71b4bf38512dbee7fb77dde957a957a03f2c1408
abhishek sharma <abhisheksharma91843@gmail.com>
2026-05-14 18:48:49 +0530

    Add Hero section in Products component

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='container text-center' style={{padding:"6rem 0"}}>
+      <h1 style={{fontSize:"1.75rem" , fontWeight:"500" , color:"#424242"}}>Zerodha Products</h1>
+      <h3 style={{fontSize:"1.25rem" , fontWeight:"400" , color:"#424242" , marginTop:"20px"}}>Sleek, modern, and intuitive trading platforms</h3>
+      <p style={{fontSize:"1rem"  , color:"#424242",marginTop:"20px"}}>Check out our <a href="" style={{textDecoration:'none'}}> investment offerings <i class="fa fa-long-arrow-right" aria-hidden="true"></i> </a></p>
       
     </div>
   )
